09409d62e56f80398ed5dbfb16a6a6185b90c993
moondiverc <nezzaluna10@gmail.com>
2026-01-26 00:06:46 +0700

    feat: auth bloc (state management)

---
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -0,0 +1,13 @@
+part of 'auth_bloc.dart';
+
+@immutable
+sealed class AuthEvent {}
+
+final class AuthSignUp extends AuthEvent {
+  // so that we can call it in auth bloc
+  final String name;
+  final String email;
+  final String password;
+
+  AuthSignUp({required this.name, required this.email, required this.password});
+}
