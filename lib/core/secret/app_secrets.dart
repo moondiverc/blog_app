72a0ab1b8548c737bc4bc6b84f297144d92c5222
moondiverc <nezzaluna10@gmail.com>
2026-01-26 22:07:49 +0700

    fix: key

--- lib/core/secret/app_secrets.dart
+++ lib/core/secret/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
   static const supabaseUrl = "https://rgkwlvqeiduegsxtenks.supabase.co";
   static const supabaseAnonKey =
-      "sb_publishable_1-cWFEoj4O9jsKCPSOjZpw_b_5a4wCt";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJna3dsdnFlaWR1ZWdzeHRlbmtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2MjI3MTEsImV4cCI6MjA4MzE5ODcxMX0.c7AegNFVoGZXdbPO9bi4Hm48C1ZYI7UNe5_sPnOyLnE";
 }
