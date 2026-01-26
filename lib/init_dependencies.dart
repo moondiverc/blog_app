ca2f9d0b02e9e16a29c57f525e8700e4c33f8c9e
moondiverc <nezzaluna10@gmail.com>
2026-01-27 00:37:18 +0700

    feat: dependency injection

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,40 @@
+import 'package:blog_app/core/secret/app_secrets.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+// register all dependencies here
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  // initialize supabase
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+// TODO: init auth dependencies
+void _initAuth() {
+  // returning AuthRemoteDataSource
+  serviceLocator.registerFactory<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(serviceLocator()),
+  );
+
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(serviceLocator()),
+  );
+
+  serviceLocator.registerFactory(() => UserSignUp(serviceLocator()));
+
+  // only want one state of AuthBloc all over the app
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(userSignUp: serviceLocator()),
+  );
+}
