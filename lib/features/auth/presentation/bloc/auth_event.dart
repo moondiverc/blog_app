f53fa0a3333db0cb858ea7130318b2ea14990fcd
moondiverc <nezzaluna10@gmail.com>
2026-01-28 23:12:08 +0700

    feat: login features

--- lib/features/auth/presentation/bloc/auth_event.dart
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -11,3 +11,11 @@ final class AuthSignUp extends AuthEvent {
 
   AuthSignUp({required this.name, required this.email, required this.password});
 }
+
+final class AuthLogin extends AuthEvent {
+  // so that we can call it in auth bloc
+  final String email;
+  final String password;
+
+  AuthLogin({required this.email, required this.password});
+}
--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,6 +2,7 @@ import 'package:blog_app/core/secret/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -33,8 +34,10 @@ void _initAuth() {
 
   serviceLocator.registerFactory(() => UserSignUp(serviceLocator()));
 
+  serviceLocator.registerFactory(() => UserLogin(serviceLocator()));
+
   // only want one state of AuthBloc all over the app
   serviceLocator.registerLazySingleton(
-    () => AuthBloc(userSignUp: serviceLocator()),
+    () => AuthBloc(userSignUp: serviceLocator(), userLogin: serviceLocator()),
   );
 }
