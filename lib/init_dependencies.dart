375ca2474d98fae64f19eab808e116b7dac4d28e
moondiverc <nezzaluna10@gmail.com>
2026-02-09 01:02:28 +0700

    feat: uplaoding blog features(model, entity, datasource, repository)

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,7 +2,7 @@ import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_app/core/secret/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/repositories/auth_repository.dart';
 import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
