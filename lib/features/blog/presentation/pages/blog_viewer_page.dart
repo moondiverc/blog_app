c4fcd4bdaf9502e93fc33d86ecb2c09015b443de
moondiverc <nezzaluna10@gmail.com>
2026-02-15 01:12:19 +0700

    feat: date formatting + scrollbar

--- lib/features/blog/presentation/pages/blog_viewer_page.dart
+++ lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -1,5 +1,6 @@
 import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/core/utils/calcuulate_reading_time.dart';
+import 'package:blog_app/core/utils/format_date.dart';
 import 'package:blog_app/features/blog/domain/entities/blog.dart';
 import 'package:flutter/material.dart';
 
@@ -13,55 +14,57 @@ class BlogViewerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                blog.title,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  blog.title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: 20.0),
+                const SizedBox(height: 20.0),
 
-              Text(
-                'By ${blog.posterName}',
-                style: const TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 16,
+                Text(
+                  'By ${blog.posterName}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: 5),
+                const SizedBox(height: 5),
 
-              Text(
-                '${blog.updatedAt}. ${calculateReadingTime(blog.content)} min',
-                style: TextStyle(
-                  color: AppPallete.greyColor,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+                Text(
+                  '${formatDateBydMMMYYYY(blog.updatedAt)}. ${calculateReadingTime(blog.content)} min',
+                  style: TextStyle(
+                    color: AppPallete.greyColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: 20.0),
+                const SizedBox(height: 20.0),
 
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(blog.imageUrl),
-              ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(blog.imageUrl),
+                ),
 
-              const SizedBox(height: 20.0),
+                const SizedBox(height: 20.0),
 
-              Text(
-                blog.content,
-                style: const TextStyle(fontSize: 16, height: 2),
-              ),
-            ],
+                Text(
+                  blog.content,
+                  style: const TextStyle(fontSize: 16, height: 2),
+                ),
+              ],
+            ),
           ),
         ),
       ),
